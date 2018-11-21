7f90c06005fb87df2946f413abe7c09b1e16041e
juliaJS <kiseleva.js@gmail.com>
2018-11-21 19:07:14 +0700

    Итерация по массивам

--- index.php
+++ index.php
@@ -61,7 +61,21 @@ $user_avatar = 'img/user.jpg';
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                <a class="promo__link" href="pages/all-lots.html">
+<?php
+$categories = [ "Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное" ];
+
+$index = 0;
+$num_count = count($categories);
+
+while($index < $num_count) {
+  print($categories[$index];
+  print('<br>');
+
+  $index = $index + 1;
+}
+?> 
+				</a>
             </li>
         </ul>
     </section>
@@ -69,18 +83,64 @@ $user_avatar = 'img/user.jpg';
         <div class="lots__header">
             <h2>Открытые лоты</h2>
         </div>
+		
+<?php
+$option_list = [
+    [
+       'name' => '2014 Rossignol District Snowboard',
+	   'category' => 'Доски и лыжи',
+	   'price' => 10999,
+	   'picture' => 'img/lot-1.jpg'
+    ],
+
+    [
+       'name' => 'DC Ply Mens 2016/2017 Snowboard',
+	   'category' => 'Доски и лыжи',
+	   'price' => 159999,
+	   'picture' => 'img/lot-2.jpg'
+    ],
+
+    [
+       'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+	   'category' => 'Крепления',
+	   'price' => 8000,
+	   'picture' => 'img/lot-3.jpg'
+    ],
+
+    [
+       'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+	   'category' => 'Ботинки',
+	   'price' => '10999',
+	   'picture' => 'img/lot-4.jpg'
+    ],
+
+    [
+       'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+	   'category' => 'Одежда',
+	   'price' => '7500',
+	   'picture' => 'img/lot-5.jpg'
+    ],
+
+    [
+       'name' => 'Маска Oakley Canopy',
+	   'category' => 'Разное',
+	   'price' => '5400',
+	   'picture' => 'img/lot-6.jpg'
+    ];
+?>
         <ul class="lots__list">
+		 <?php foreach ($option_list as $key => $item) : ?>
             <!--заполните этот список из массива с товарами-->
             <li class="lots__item lot">
                 <div class="lot__image">
                     <img src="" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$item['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__amount"><?=$item['price']; ?></span>
                             <span class="lot__cost">цена<b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
@@ -89,6 +149,7 @@ $user_avatar = 'img/user.jpg';
                     </div>
                 </div>
             </li>
+			<?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -99,7 +160,21 @@ $user_avatar = 'img/user.jpg';
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
             <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+                <a href="pages/all-lots.html">
+<?php
+$categories = [ "Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное" ];
+
+$index = 0;
+$num_count = count($categories);
+
+while($index < $num_count) {
+  print($categories[$index];
+  print('<br>');
+
+  $index = $index + 1;
+}
+?>
+				</a>
             </li>
         </ul>
     </nav>
