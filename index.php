f3dbb0d27e0afa8c8e2cb86d9d6bff2df8bdec43
juliaJS <kiseleva.js@gmail.com>
2018-11-30 13:12:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,43 +7,38 @@ $user_avatar = 'img/user.jpg';
 $categories = [ "Доски и лыжи" , "Крепления" , "Ботинки" , "Одежда" , "Инструменты" , "Разное" ];
 
 $option_list = [
-    [
-       'name' => '2014 Rossignol District Snowboard',
-	   'category' => 'Доски и лыжи',
-	   'price' => 10999,
-	   'picture' => 'img/lot-1.jpg'
+
+    [  	'name' => '2014 Rossignol District Snowboard',
+		'category' => 'Доски и лыжи',
+		'price' => 10999,
+		'picture' => 'img/lot-1.jpg'
     ],
 
-    [
-       'name' => 'DC Ply Mens 2016/2017 Snowboard',
-	   'category' => 'Доски и лыжи',
-	   'price' => 159999,
-	   'picture' => 'img/lot-2.jpg'
+    [	'name' => 'DC Ply Mens 2016/2017 Snowboard',
+		'category' => 'Доски и лыжи',
+		'price' => 159999,
+		'picture' => 'img/lot-2.jpg'
     ],
 
-    [
-       'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-	   'category' => 'Крепления',
-	   'price' => 8000,
-	   'picture' => 'img/lot-3.jpg'
+    [	'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+		'category' => 'Крепления',
+		'price' => 8000,
+		'picture' => 'img/lot-3.jpg'
     ],
 
-    [
-       'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-	   'category' => 'Ботинки',
-	   'price' => 10999,
-	   'picture' => 'img/lot-4.jpg'
+    [	'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+		'category' => 'Ботинки',
+		'price' => 10999,
+		'picture' => 'img/lot-4.jpg'
     ],
 
-    [
-       'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+    [  'name' => 'Куртка для сноуборда DC Mutiny Charocal',
 	   'category' => 'Одежда',
 	   'price' => 7500,
 	   'picture' => 'img/lot-5.jpg'
     ],
 
-    [
-       'name' => 'Маска Oakley Canopy',
+    [  'name' => 'Маска Oakley Canopy',
 	   'category' => 'Разное',
 	   'price' => 5400,
 	   'picture' => 'img/lot-6.jpg'
@@ -55,7 +50,7 @@ function cut_num($price)
 	$num = ceil($price);
 	
 	if ($num > 1000) {
-	    $price = number_format($num, 0, '', ' ');
+	    $price = number_format($num, 0, null, ' ');
 		$price .= " ₽";
 	}
 	return $price;
@@ -140,7 +135,7 @@ function cut_num($price)
                     <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount"><?=cut_num($item['price']); ?></span>
+                            <span class="lot__amount">Стартовая цена</span>
                             <span class="lot__cost"><?=cut_num($item['price']); ?></span>
                         </div>
                         <div class="lot__timer timer">
