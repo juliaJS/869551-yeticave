f76edf5563ea911d7f984a4c506f615bbdaaa866
juliaJS <kiseleva.js@gmail.com>
2018-12-10 14:51:54 +0700

    Пишем SQL-запросы.

--- queries.sql
+++ queries.sql
@@ -19,17 +19,17 @@ INSERT INTO users
 
 INSERT INTO lots
     (title, description, image, price, user_id, category_id, increment_rate, end_time) VALUES
-      ('2014 Rossignol District Snowboard', 'Сноуборд из платика. Фрирайд', 'img/lot-1.jpg', 10999, 1, 1, 200,
+      ('2014 Rossignol District Snowboard', 'Сноуборд из берёзы. Фрирайд', 'img/lot-1.jpg', 10999, 1, 1, 200,
             '2018-12-20 00:33:22'),
-      ('DC Ply Mens 2016/2017 Snowboard', 'Сноуборд из платика. Фрирайд', 'img/lot-2.jpg', 159999, 2, 1, 200,
+      ('DC Ply Mens 2016/2017 Snowboard', 'Сноуборд из берёзы. Фрирайд', 'img/lot-2.jpg', 159999, 2, 1, 200,
             '2018-12-20 00:33:22'),
-      ('Крепления Union Contact Pro 2015 года размер L/XL', 'Сноуборд из платика. Фрирайд', 'img/lot-3.jpg', 8000,
+      ('Крепления Union Contact Pro 2015 года размер L/XL', 'Сноуборд из берёзы. Фрирайд', 'img/lot-3.jpg', 8000,
             3, 2, 200, '2018-12-20 00:33:22'),
-      ('Ботинки для сноуборда DC Mutiny Charocal', 'Сноуборд из платика. Фрирайд', 'img/lot-4.jpg', 10999, 1, 4, 200,
+      ('Ботинки для сноуборда DC Mutiny Charocal', 'Сноуборд из берёзы. Фрирайд', 'img/lot-4.jpg', 10999, 1, 4, 200,
             '2018-12-20 00:33:22'),
-      ('Куртка для сноуборда DC Mutiny Charocal', 'Сноуборд из платика. Фрирайд', 'img/lot-5.jpg', 7500, 2, 5, 200,
+      ('Куртка для сноуборда DC Mutiny Charocal', 'Сноуборд из берёзы. Фрирайд', 'img/lot-5.jpg', 7500, 2, 5, 200,
             '2018-12-20 00:33:22'),
-      ('Маска Oakley Canopy', 'Сноуборд из платика. Фрирайд', 'img/lot-6.jpg', 5400, 3, 6, 200, '2018-12-20 00:33:22');
+      ('Маска Oakley Canopy', 'Сноуборд из берёзы. Фрирайд', 'img/lot-6.jpg', 5400, 3, 6, 200, '2018-12-20 00:33:22');
 	
 	
 INSERT INTO rates
@@ -51,14 +51,15 @@ INSERT INTO rates
 --получение списка самых свежих ставок для лота по его идентификатору
 SELECT * FROM rates WHERE lot_id = 6;
 
+--обновить название лота по его идентификатору
+UPDATE lots SET description = 'Сноуборд. Фрирайд' WHERE id = 1;
+UPDATE lots SET description = 'Сноуборд. Фрирайд' WHERE id = 2;
+UPDATE lots SET description = 'Крепления. Жеские' WHERE id = 3;
+UPDATE lots SET description = 'Ботинки. Размер 40' WHERE id = 4;
+UPDATE lots SET description = 'Куртка. Размер S' WHERE id = 5;
+UPDATE lots SET description = 'Маска' WHERE id = 6;
+
 --показ лота по его id, а также получение названия категории, к которой принадлежит лот
 SELECT l.id, l.title, l.price, l.image, c.name
-FROM lots l WHERE l.id = 5
-  LEFT JOIN categories c ON c.id = l.category_id;
-
---обновить название лота по его идентификатору
-UPDATE lots SET name = '2018 Rossignol District Snowboard'
-WHERE id = 1;
-
-
-
+FROM lots l
+LEFT JOIN categories c ON c.id = l.category_id WHERE l.id = 5;
